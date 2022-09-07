eae3d8dae5ff0e4879c33f96c1f0505a7b2a4343
hgetov <h.getov@datamax.bg>
2022-09-07 16:41:18 +0300

    Changes to update

--- app/src/main/java/com/example/shopinglistapp/Controllers/MainActivity.java
+++ app/src/main/java/com/example/shopinglistapp/Controllers/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.shopinglistapp.R;
 import com.github.javiersantos.appupdater.AppUpdater;
@@ -17,19 +20,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        Log.e("LOG", "START APP");
         AppUpdater appUpdater = new AppUpdater(this);
         appUpdater.setUpdateFrom(UpdateFrom.JSON)
-                .setUpdateJSON("https://raw.githubusercontent.com/hristogetov/ShopingListApp/master/app/shoping_list.json")
+                .setUpdateJSON("https://raw.githubusercontent.com/hristogetov/ShopingListApp/master/app/update-changelog.json")
                 .setDisplay(Display.DIALOG);//.setDuration(Duration.NORMAL);
         appUpdater.setTitleOnUpdateAvailable("Update available")
                 .setContentOnUpdateAvailable("Check out the latest version available of my app!")
                 .setTitleOnUpdateNotAvailable("Update not available")
                 .setContentOnUpdateNotAvailable("No update available. Check for updates again later!")
                 .setButtonUpdate("Update now?")
-                .setIcon(R.drawable.add_button) // Notification icon
+                .setIcon(R.drawable.ic_launcher_foreground) // Notification icon
                 .setCancelable(false);
         appUpdater.start();
+
+        Button newItem = findViewById(R.id.new_item);
+        newItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NewItemActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button shoppingList = findViewById(R.id.list_button);
+        shoppingList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ShoppingListActivity.class);
+                startActivity(intent);
+            }
+        });
+       // Toast.makeText(this,"Updated successfully", Toast.LENGTH_LONG).show();
         getSupportActionBar().hide();
     }
 
